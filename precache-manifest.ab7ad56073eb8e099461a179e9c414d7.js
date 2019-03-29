7ad2efcd76288f24e8d86eedc47eeb93988ceb3c
kinshuk <kinshuksharma311@gmail.com>
2019-03-29 21:38:32 +0530

    Updates

---
+++ precache-manifest.ab7ad56073eb8e099461a179e9c414d7.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "3599c796e25aff1c0f5377baea14f1f2",
+    "url": "/RoyalBrothers/static/media/b3.3599c796.png"
+  },
+  {
+    "revision": "d2add47dc60db35d113e",
+    "url": "/RoyalBrothers/static/css/main.266e5981.chunk.css"
+  },
+  {
+    "revision": "5a9dce2b30ecc12b6a50",
+    "url": "/RoyalBrothers/static/js/runtime~main.eb0804a0.js"
+  },
+  {
+    "revision": "cb69963dd132c94db0d72c1b6043cb69",
+    "url": "/RoyalBrothers/static/media/BgImage.cb69963d.jpg"
+  },
+  {
+    "revision": "147ff708e5145365235f",
+    "url": "/RoyalBrothers/static/js/2.943b2dd6.chunk.js"
+  },
+  {
+    "revision": "1b028c4b7807ad0f9d2b65d39a21a6d3",
+    "url": "/RoyalBrothers/static/media/rbLogo.1b028c4b.png"
+  },
+  {
+    "revision": "ac4471d5f4b4df9ca8bd7721a351b618",
+    "url": "/RoyalBrothers/static/media/city.ac4471d5.JPG"
+  },
+  {
+    "revision": "76416a98505fc063c2f623e180991112",
+    "url": "/RoyalBrothers/static/media/km.76416a98.JPG"
+  },
+  {
+    "revision": "49ca11d6aa7df7fefdbcf5a23846d572",
+    "url": "/RoyalBrothers/static/media/fb.49ca11d6.JPG"
+  },
+  {
+    "revision": "12f65528bfadf89d73cfcbf95b33be9f",
+    "url": "/RoyalBrothers/static/media/google.12f65528.JPG"
+  },
+  {
+    "revision": "ef4f7744db31cc2d7441e5f8fc2c9290",
+    "url": "/RoyalBrothers/static/media/b1.ef4f7744.png"
+  },
+  {
+    "revision": "c34ee6e631c5c2ead30238f1847eaf42",
+    "url": "/RoyalBrothers/static/media/b2.c34ee6e6.png"
+  },
+  {
+    "revision": "d2add47dc60db35d113e",
+    "url": "/RoyalBrothers/static/js/main.645cc34e.chunk.js"
+  },
+  {
+    "revision": "63cd77081479a5d171f9915afc720482",
+    "url": "/RoyalBrothers/static/media/b4.63cd7708.png"
+  },
+  {
+    "revision": "1edfabb3aa2e89b562f745143b558f77",
+    "url": "/RoyalBrothers/static/media/b5.1edfabb3.png"
+  },
+  {
+    "revision": "d5044d9dbaa5c5f41c1b9151c15b2741",
+    "url": "/RoyalBrothers/static/media/b6.d5044d9d.png"
+  },
+  {
+    "revision": "dac185982dd0efb50691f76e3c64d1ce",
+    "url": "/RoyalBrothers/static/media/b7.dac18598.png"
+  },
+  {
+    "revision": "86b1ec9f85d4a3fa7ff5b8637f65b614",
+    "url": "/RoyalBrothers/static/media/b8.86b1ec9f.png"
+  },
+  {
+    "revision": "c417bf105264cb69c244b835f92cc1cb",
+    "url": "/RoyalBrothers/static/media/b9.c417bf10.png"
+  },
+  {
+    "revision": "fb2baf6f21483f8d0aafe0772f9d054b",
+    "url": "/RoyalBrothers/static/media/b10.fb2baf6f.png"
+  },
+  {
+    "revision": "755b6f669ee74366878a27cd98b52714",
+    "url": "/RoyalBrothers/static/media/b11.755b6f66.png"
+  },
+  {
+    "revision": "a02d3033226138529eb8b4ce80388601",
+    "url": "/RoyalBrothers/static/media/yourStory1.a02d3033.png"
+  },
+  {
+    "revision": "b3f7cb51d3d3ab0a0ee362c616e3c71c",
+    "url": "/RoyalBrothers/static/media/hindu1.b3f7cb51.png"
+  },
+  {
+    "revision": "147ff708e5145365235f",
+    "url": "/RoyalBrothers/static/css/2.8360ce7e.chunk.css"
+  },
+  {
+    "revision": "2742ab338aa1dd979171f8f9458341f9",
+    "url": "/RoyalBrothers/index.html"
+  }
+];
